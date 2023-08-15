d20728870c8cf7ddad9babc81bbaf1b381085f31
TOUFIQUER RAHMAN <toufiquerabc@gmail.com>
2023-08-16 05:58:11 +0600

    done @ add toggle in logIn and log OUt page

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,9 +1,31 @@
-import React from "react";
-
+"use client";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 const LogIn = () => {
+  const [inputType, setInputType] = useState("password");
+  const toggleType = () => {
+    if (inputType === "password") {
+      setInputType("text");
+    } else {
+      setInputType("password");
+    }
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data, " => Line No: 5");
+  };
   return (
-    <form className={` w-full flex items-center justify-center`}>
-      <div className=" bg-slate-800 card h-auto p-8 pt-12 card min-w-[500px]">
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className={` w-full flex items-center justify-center`}
+    >
+      <div className=" bg-slate-800 card h-auto p-8 pt-12  min-w-[500px]">
         {/* Email */}
         <div className="mb-6">
           <label
@@ -12,28 +34,49 @@ const LogIn = () => {
           >
             Your email
           </label>
+
+          {/* include validation with required or other standard HTML validation rules */}
           <input
-            type="email"
-            id="email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@flowbite.com"
-            required
+            placeholder="name@example.com"
+            {...register("email", { required: true })}
           />
+          {/* errors will return when field validation fails  */}
+          {errors.email && <span>This field is required</span>}
         </div>
         {/* Password */}
-        <div className="mb-6">
+        <div className="mb-6 relative">
           <label
             htmlFor="password"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Your password
           </label>
+
+          {/* include validation with required or other standard HTML validation rules */}
           <input
-            type="password"
-            id="password"
+            type={inputType}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required
+            {...register("password", { required: true })}
+            placeholder="Your password"
           />
+          <div className="absolute right-4 top-[38px]">
+            {inputType === "password" ? (
+              <AiFillEyeInvisible
+                onClick={toggleType}
+                className={` h-6 w-6 cursor-pointer`}
+              />
+            ) : (
+              <AiFillEye
+                onClick={toggleType}
+                className={` h-6 w-6 cursor-pointer`}
+              />
+            )}
+          </div>
+          {/* errors will return when field validation fails  */}
+          {errors.password && (
+            <span className="text-red-500">This field is required</span>
+          )}
         </div>
         {/* Submit */}
         <button
--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,9 +1,37 @@
-import React from "react";
-
+"use client";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 const Registration = () => {
+  const [inputType, setInputType] = useState("password");
+  const [error, setError] = useState({ isError: false, message: "" });
+  const toggleType = () => {
+    if (inputType === "password") {
+      setInputType("text");
+    } else {
+      setInputType("password");
+    }
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    if (data.password !== data.conPassword) {
+      setError({ isError: true, message: "Password doesn't match" });
+    } else {
+      setError({ isError: false, message: "" });
+      console.log(data, " => Line No: 5");
+    }
+  };
   return (
-    <form className={` w-full flex items-center justify-center`}>
-      <div className=" bg-slate-800 card h-auto p-8 pt-12 card min-w-[500px]">
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className={` w-full flex items-center justify-center`}
+    >
+      <div className=" bg-slate-800 card h-auto p-8 pt-12  min-w-[500px]">
         {/* Email */}
         <div className="mb-6">
           <label
@@ -12,31 +40,52 @@ const Registration = () => {
           >
             Your email
           </label>
+
+          {/* include validation with required or other standard HTML validation rules */}
           <input
-            type="email"
-            id="email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@flowbite.com"
-            required
+            placeholder="name@example.com"
+            {...register("email", { required: true })}
           />
+          {/* errors will return when field validation fails  */}
+          {errors.email && <span>This field is required</span>}
         </div>
         {/* Password */}
-        <div className="mb-6">
+        <div className="mb-6 relative">
           <label
             htmlFor="password"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Your password
           </label>
+
+          {/* include validation with required or other standard HTML validation rules */}
           <input
-            type="password"
-            id="password"
+            type={inputType}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required
+            {...register("password", { required: true })}
+            placeholder="Your password"
           />
+          <div className="absolute right-4 top-[38px]">
+            {inputType === "password" ? (
+              <AiFillEyeInvisible
+                onClick={toggleType}
+                className={` h-6 w-6 cursor-pointer`}
+              />
+            ) : (
+              <AiFillEye
+                onClick={toggleType}
+                className={` h-6 w-6 cursor-pointer`}
+              />
+            )}
+          </div>
+          {/* errors will return when field validation fails  */}
+          {errors.password && (
+            <span className="text-red-500">This field is required</span>
+          )}
         </div>
         {/* Confirm Password */}
-        <div className="mb-6">
+        <div className="mb-6 relative">
           <label
             htmlFor="password"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -44,12 +93,31 @@ const Registration = () => {
             Confirm Password
           </label>
           <input
-            type="password"
-            id="conPassword"
-            placeholder="Confirm Password"
+            type={inputType}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required
-          />
+            {...register("conPassword", { required: true })}
+            placeholder="Your password"
+          />{" "}
+          <div className="absolute right-4 top-[38px]">
+            {inputType === "password" ? (
+              <AiFillEyeInvisible
+                onClick={toggleType}
+                className={` h-6 w-6 cursor-pointer`}
+              />
+            ) : (
+              <AiFillEye
+                onClick={toggleType}
+                className={` h-6 w-6 cursor-pointer`}
+              />
+            )}
+          </div>
+          {/* errors will return when field validation fails  */}
+          {errors.conPassword && (
+            <span className="text-red-500">This field is required</span>
+          )}
+          {error.isError && (
+            <span className="text-red-500">{error.message}</span>
+          )}
         </div>
         {/* Submit */}
         <button
